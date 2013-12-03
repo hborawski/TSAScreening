803d18879bf315152c77836f95cbce9ba62c8ec2
Skuloth <skuloth@gmail.com>
2013-12-02 22:31:17 -0500

    BagScanner
    Wrote bag scanner class, currently does not send the message to the
    Security class because I dont currently know how the scanner will know
    about its security overlord.

--- src/BagScanner.java
+++ src/BagScanner.java
@@ -1,11 +1,24 @@
 import akka.actor.UntypedActor;
+import java.util.Random;
 
 
 public class BagScanner extends UntypedActor{
-
+	private Random ran = new Random();
+	private int ID;
+	private int BagID;
+	private PassengerBagCheck;
 	@Override
-	public void onReceive(Object arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void onReceive(Object message) throws Exception {
+		if(message instanceof CheckBag){
+			BagID = message.getBagID();
+			if(ran.nextInt(10) >= 2){
+				PassengerBagCheck = new PassengerBagCheck(BagID, true);
+				System.out.println("Bag Scanner: " + ID + " passed bagID " + BagID);
+			}else{
+				PassengerBagCheck = new PassengerBagCheck(BagID, false);
+				System.out.println("Bag Scanner: " + ID + " failed bagID " + BagID);
+			}
+		}
 		
 	}
 
